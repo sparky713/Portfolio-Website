fa600ddb0b068dfd84ac11a3d059bd569cf28fc1
Sally Park <sallypark73@hotmail.com>
2022-02-18 11:54:57 -0800

    added parallox scroll effect and music playlist

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar_menu')
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar_menu");
 
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+
+let moon = document.getElementById("moon");
+let bg = document.getElementById("background");
+
+window.addEventListener("scroll", function () {
+  const value = window.scrollY;
+
+  moon.style.left = -value * 0.5 + "px";
+  moon.style.top = value * 0.8 + "px";
+//   bg.style.top = -value * 0.5 + "px";
 });
